14680cce58d41bad4ec9037cd7d84db11227679f
Vivianne Melo <viviannemmelo@gmail.com>
2026-07-07 06:28:48 -0300

    structure and testing of approved order

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,9 +1,8 @@
 import { test, expect } from '@playwright/test'
 
-test('web apps deve estar online', async ({ page }) => {
+test('webapp deve estar online', async ({ page }) => {
   await page.goto('http://localhost:5173')
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Velô by Papito/)
 })
-
---
+++ playwright/e2e/pedidos.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test'
+
+test('deve consultar um pedido aprovado', async ({ page }) => {
+  await page.goto('http://localhost:5173')
+
+  // Checkpoint 
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+
+  // Checkpoint 
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+
+  // Checkpoint 
+  await page.getByTestId('search-order-id').fill('VLO-2WBIEG')
+  await page.getByTestId('search-order-button').click()
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-2WBIEG')
+
+  await expect(page.getByTestId('order-result-status')).toBeVisible()
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+})
