1bc2e27968463bf40abb66be1b74dfdc2c1c87e5
Vivianne Melo <viviannemmelo@gmail.com>
2026-07-14 05:55:22 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,18 +1,22 @@
 import { test, expect } from '@playwright/test'
 
+/// AAA - Arrange, Act, Assert
+
 test('deve consultar um pedido aprovado', async ({ page }) => {
+
+  // Arrange
   await page.goto('http://localhost:5173')
-
-  // Checkpoint 
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
 
-  // Checkpoint 
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
-  // Checkpoint 
+  // Act
   await page.getByTestId('search-order-id').fill('VLO-2WBIEG')
   await page.getByTestId('search-order-button').click()
+
+  // Assert
+  await expect(page.getByTestId('order-result-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-2WBIEG')
 
   await expect(page.getByTestId('order-result-status')).toBeVisible()
