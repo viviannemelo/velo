148aec102e9d1f3ed083436cbef336de5c4b94a9
Vivianne Melo <viviannemmelo@gmail.com>
2026-07-21 06:25:06 -0300

    code challenge: new locator

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -12,13 +12,13 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
   // Act
-  await page.getByTestId('search-order-id').fill('VLO-2WBIEG')
-  await page.getByTestId('search-order-button').click()
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-2WBIEG')
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+
 
   // Assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible()
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-2WBIEG')
+  await expect(page.getByText('Pedido', { exact: true })).toBeVisible({ timeout: 10_000 })
+  await expect(page.locator('div').filter({ hasText: /^PedidoVLO-2WBIEG$/ }).first()).toBeVisible()
 
-  await expect(page.getByTestId('order-result-status')).toBeVisible()
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+  await expect(page.getByText('APROVADO')).toBeVisible()
 })
